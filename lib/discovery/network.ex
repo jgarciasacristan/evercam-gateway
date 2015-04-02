6f410d449f7c57be620cf0576fda60381c6f48d3
Daniel Caffrey <scandox@gmail.com>
2015-04-02 16:43:26 +0100

    Finished v1.0 of Network scan code

--- lib/discovery/network.ex
+++ lib/discovery/network.ex
@@ -4,16 +4,27 @@ defmodule Gateway.Discovery.Network do
   @doc "Scan specified target network (i.e. 192.168.0.1-192.168.100.255) 
   on specified NIC. Default target is local network." 
   def scan(interface, target \\ "--localnet", arp_scan_options \\ "") do
-    alias Porcelain.Result
     command = "sudo arp-scan " <> arp_scan_options <> " --interface " <> 
               interface <> " " <> target
-    %Result{out: output, status: status} = Porcelain.shell(command)
-    parse_scan(output)
+    %Porcelain.Result{out: output, status: status} = Porcelain.shell(command)
+    output |> parse_scan
+  end
+
+  @doc "Scan all NICs for target network. Default is local network 
+  for each interface."
+  def scan_all(target \\ "--localnet", arp_scan_options \\ "") do
+    get_interfaces |> Enum.map(&(scan(&1,target,arp_scan_options))) |> List.flatten()
   end
 
   # Parse arp-scan results into an idiomatic format
   defp parse_scan(results) do 
-    results
+    Regex.scan(~r/(?<ip>(?:\d{1,3}\.){3}\d{1,3})\t(?<mac>(?:[0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2})/, results, capture: :all_names)
+  end
+
+  # Get a list of all the Network interfaces, discard local loopback
+  defp get_interfaces() do
+    {:ok, interface_data} = :inet.getifaddrs()
+    Enum.filter_map(interface_data, &(elem(&1,0) != 'lo'), &(to_string elem(&1,0)))    
   end
 
 end
