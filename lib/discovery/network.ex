62afc1d3f472eb6cecf193d38c33b1da1b005f6f
Daniel Caffrey <scandox@gmail.com>
2015-04-07 14:40:06 +0100

    Scan now returns a list of tuples instead of a list of lists. Also added support to return human readable network interface data.

--- lib/discovery/network.ex
+++ lib/discovery/network.ex
@@ -7,13 +7,24 @@ defmodule Gateway.Discovery.Network do
     command = "sudo arp-scan " <> arp_scan_options <> " --interface " <> 
               interface <> " " <> target
     %Porcelain.Result{out: output, status: status} = Porcelain.shell(command)
-    output |> parse_scan
+    output 
+      |> parse_scan
+      |> Enum.map(&(List.to_tuple(&1)))
   end
 
   @doc "Scan all NICs for target network. Default is local network 
   for each interface."
   def scan_all(target \\ "--localnet", arp_scan_options \\ "") do
-    get_interfaces |> Enum.map(&(scan(&1,target,arp_scan_options))) |> List.flatten()
+    get_interfaces
+      |> Enum.map(&(to_string elem(&1,0)))
+      |> Enum.map(&(scan(&1,target,arp_scan_options))) 
+      |> List.flatten()
+  end
+
+  @doc "Get data for local networks"
+  def get_networks() do
+    get_interfaces
+      |> parse_interfaces
   end
 
   # Parse arp-scan results into an idiomatic format
@@ -21,10 +32,55 @@ defmodule Gateway.Discovery.Network do
     Regex.scan(~r/(?<ip>(?:\d{1,3}\.){3}\d{1,3})\t(?<mac>(?:[0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2})/, results, capture: :all_names)
   end
 
+  # Parse interface data. Transform IP addresses and MAC Addresses to string representations
+  defp parse_interfaces(interfaces) do
+    interfaces 
+      |> Enum.map(&(parse_interface(&1)))
+  end
+
+  # Parse a specific interface. TODO: find a nicer way to replace elements.
+  # TODO: Handle the Ipv6 addr which just happens to have the same bloody key-name
+  defp parse_interface(interface) do
+    IO.inspect interface
+    {interface_name, elements} = interface
+    {to_string(interface_name), Enum.map(elements, fn(x) -> replace_key(elements, elem(x,0)) end) }
+  end
+
+  defp replace_key(elements, key) do
+    case key do
+      :addr ->
+        {key, to_ipstring(elements[key])}
+      :netmask ->
+        {key, to_ipstring(elements[key])}
+      :broadaddr ->
+        {key, to_ipstring(elements[key])}
+      :hwaddr ->
+        {key, to_macstring(elements[key])}
+      _->
+        {key, elements[key]}
+    end
+  end
+
+  # turns an erlang ip_address() into a string
+  defp to_ipstring(addr) do
+    addr
+      |> :inet.ntoa
+      |> to_string
+  end
+
+  # hwaddr (i.e. mac) is represented as a list of decimal integers. Turn into hex string with separator.
+  defp to_macstring(hwaddr) do
+    hwaddr
+      |> Enum.map(&(Hexate.encode(&1,2) <> ":"))
+      |> to_string
+      |> String.rstrip(?:)
+  end
+
   # Get a list of all the Network interfaces, discard local loopback
   defp get_interfaces() do
     {:ok, interface_data} = :inet.getifaddrs()
-    Enum.filter_map(interface_data, &(elem(&1,0) != 'lo'), &(to_string elem(&1,0)))    
+    interface_data 
+      |> Enum.filter(&(elem(&1,0) != 'lo'))    
   end
 
 end
