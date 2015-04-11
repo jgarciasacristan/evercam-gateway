bbe409164af8a556370c0a7ceeb943688fed9080
Daniel Caffrey <scandox@gmail.com>
2015-04-11 23:10:32 +0100

    Corrected comments

--- lib/utilities/parallel.ex
+++ lib/utilities/parallel.ex
@@ -3,7 +3,7 @@ defmodule Gateway.Utilities.Parallel do
   @doc "Parallel Map implementation stolen from Dave Thomas' book: 
   https://pragprog.com/book/elixir/programming-elixir. I altered it
   to protect it from the failure of the spawned processes. Also it returns
-  an empty tuple on failure. Finally I added a timeout parameter so it can be 
+  an indicative tuple on failure. Finally I added a timeout parameter so it can be 
   called with varying levels of tolerance."
   def map(collection, function, timeout) do
     me = self
