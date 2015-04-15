8dec3b9c4de42a3350c9b481d5bd7f49bd63232c
Dan <scandox@gmail.com>
2015-04-15 23:44:47 +0100

    Wrote a generic Stash server for keeping backup of other processes state, in case of abnormal termination

---
+++ lib/utilities/stash.ex
@@ -0,0 +1,31 @@
+defmodule Gateway.Utilities.Stash do
+  @moduledoc "A module designed to keep backup state of other processes"
+  use GenServer
+
+  ## Client API
+
+  def start_link do
+    GenServer.start_link(__MODULE__,[])
+  end
+
+  @doc "Stashes any value. Replaces any value previously stashed."
+  def stash(pid, value) do
+    GenServer.cast(pid, {:stash, value})
+  end
+
+  @doc "Retrieves the value stashed."
+  def retrieve(pid) do
+    GenServer.call(pid, :retrieve)
+  end
+
+  ## Server Callbacks
+
+  def handle_call(:retrieve, _from, current_value) do
+    {:reply, current_value, current_value}
+  end
+
+  def handle_cast({:stash, value}, current_value) do
+    {:noreply, value}
+  end
+
+end
