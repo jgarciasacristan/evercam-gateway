cfa4b76d849beb096424557e398cf1e69a0de9c1
Daniel Caffrey <scandox@gmail.com>
2015-04-27 11:50:01 +0100

    Updated Host Discovery to scan all ports. Adjusted service probes file.

--- bin/install-nmap-config.sh
+++ bin/install-nmap-config.sh
@@ -2,4 +2,4 @@
 
 # Ensures Nmap configuration file is present. NMAPDIR is set in environment
 [ ! -d ~/.nmap ] && mkdir ~/.nmap
-cp -f ../nmap-service-probes ~/.nmap
+cp -f nmap-service-probes ~/.nmap
--- lib/discovery/host.ex
+++ lib/discovery/host.ex
@@ -2,9 +2,10 @@ defmodule Gateway.Discovery.Host do
   @moduledoc "Scans individual hosts/devices using Nmap(1)"
 
   @doc "Scans network host (i.e. 192.168.1.50) for ports and 
-  identifies services on those ports"
+  identifies services on those ports. -p- scans every port. -sV is a service scan
+  -oX is to output XML."
   def scan(host) do 
-    command = "nmap -sV -oX - #{host}"
+    command = "nmap -p- -sV -oX - #{host}"
     %Porcelain.Result{out: output, status: _status} = Porcelain.shell(command)
     output
       |> parse_scan
@@ -13,6 +14,8 @@ defmodule Gateway.Discovery.Host do
   # Parses XML output of Nmap - no JSON from nmap yet sadly.
   # FIXME: This is fragile and ugly. Replace either using customised
   # SAX parser in erlsom, or create an XSL for Nmap XML to JSON
+  # TODO: Examine DTD for Nmap XML see if I can use it to be more
+  # accurate.
   defp parse_scan(xml) do
     scan_results = :erlsom.simple_form(xml)
 
