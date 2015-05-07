a3f816211a00ad05fbdfd08185c32e08347f9726
Daniel Caffrey <scandox@gmail.com>
2015-05-07 15:49:23 +0100

    Updated test to reflect changes in the function of network interface parsing

--- test/utilities/network_test.exs
+++ test/utilities/network_test.exs
@@ -4,16 +4,18 @@ defmodule Gateway.Utilities.NetworkTest do
 
   doctest Network
 
-  test "that interfaces are parsed to return string representations of IP and MAC Addresses" do
+  test "that interfaces are parsed into Map with string representations of IP and MAC Addresses" do
     assert sample_interfaces |> Network.parse_interfaces ==
-        [{"eth0",
-          [flags: [:up, :broadcast, :running, :multicast],
-          hwaddr: "00:25:90:a6:f7:8c"]},
-        {"eth1", [flags: [:broadcast, :multicast], hwaddr: "00:25:90:a6:f7:8d"]},
-        {"wlan1",
-          [flags: [:up, :broadcast, :running, :multicast], hwaddr: "10:fe:ed:1f:56:0d",
-          addr: "172.16.0.184", netmask: "255.255.255.0", broadaddr: "172.16.0.255",
-          addr: "FE80::12FE:EDFF:FE1F:560D", netmask: "FFFF:FFFF:FFFF:FFFF::"]}] 
+        [%{:flags => [:up, :broadcast, :running, :multicast],
+          "mac_address" => "00:25:90:a6:f7:8c", "name" => "eth0"},
+        %{:flags => [:broadcast, :multicast], "mac_address" => "00:25:90:a6:f7:8d",
+          "name" => "eth1"},
+        %{:flags => [:up, :broadcast, :running, :multicast],
+          "broadcast_address" => "172.16.0.255", "ip_address" => "172.16.0.184",
+          "ipv6_address" => "FE80::12FE:EDFF:FE1F:560D",
+          "ipv6_net_mask" => "FFFF:FFFF:FFFF:FFFF::",
+          "mac_address" => "10:fe:ed:1f:56:0d", "name" => "wlan1",
+          "net_mask" => "255.255.255.0"}]   
   end
 
   test "that interface attributes are retrieved correctly" do
