2799488c203569bacaed1230c763cc1e052b0422
Daniel Caffrey <scandox@gmail.com>
2015-05-07 14:32:25 +0100

    Removed duplicate shell function

--- lib/utilities/external.ex
+++ lib/utilities/external.ex
@@ -1,9 +1,5 @@
 defmodule Gateway.Utilities.External do
 
-  def shell(command) do
-    Porcelain.shell(command, [out: :string, err: :string])
-  end
-
   def shell(command, opts \\ [out: :string, err: :string]) do
     Porcelain.shell(command, opts)
   end
