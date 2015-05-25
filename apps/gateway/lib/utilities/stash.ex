eec3e9d20b613f9a28e0c7c631e4d9ddefc61d01
jgarciasacristan <jose.garciasacristan@gmail.com>
2015-05-25 22:36:02 +0100

    Use Agent instead of GenServer to store the state.

--- apps/gateway/lib/utilities/stash.ex
+++ apps/gateway/lib/utilities/stash.ex
@@ -1,31 +1,20 @@
 defmodule Gateway.Utilities.Stash do
   @moduledoc "A module designed to keep backup state of other processes"
-  use GenServer
 
   ## Client API
 
   def start_link do
-    GenServer.start_link(__MODULE__,[])
+    Agent.start(fn -> [] end)
   end
 
   @doc "Stashes any value. Replaces any value previously stashed."
   def stash(pid, value) do
-    GenServer.cast(pid, {:stash, value})
+    Agent.update(pid, fn -> value end)
   end
 
   @doc "Retrieves the value stashed."
   def retrieve(pid) do
-    GenServer.call(pid, :retrieve)
-  end
-
-  ## Server Callbacks
-
-  def handle_call(:retrieve, _from, current_value) do
-    {:reply, current_value, current_value}
-  end
-
-  def handle_cast({:stash, value}, _current_value) do
-    {:noreply, value}
+    Agent.get(pid, &(&1))
   end
 
 end
